0fc76b4944d35c8863df9319f27b40d56716ea7d
morakoshka <weirdwitchbrew@gmail.com>
2025-10-22 19:14:25 -0600

    galeria bd

---
+++ galeria_portafolio.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2025 at 03:12 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `galeria_portafolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categoria`
+--
+-- Error reading structure for table galeria_portafolio.categoria: #1932 - Table &#039;galeria_portafolio.categoria&#039; doesn&#039;t exist in engine
+-- Error reading data for table galeria_portafolio.categoria: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `galeria_portafolio`.`categoria`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagen`
+--
+-- Error reading structure for table galeria_portafolio.imagen: #1932 - Table &#039;galeria_portafolio.imagen&#039; doesn&#039;t exist in engine
+-- Error reading data for table galeria_portafolio.imagen: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `galeria_portafolio`.`imagen`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagen_categoria`
+--
+-- Error reading structure for table galeria_portafolio.imagen_categoria: #1932 - Table &#039;galeria_portafolio.imagen_categoria&#039; doesn&#039;t exist in engine
+-- Error reading data for table galeria_portafolio.imagen_categoria: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `galeria_portafolio`.`imagen_categoria`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
